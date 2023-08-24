919b060b0d7dbce16c59a3b8e2111f0b9dffb440
ebitech02 <ebipresh02@gmail.com>
2023-08-24 02:34:45 -0700

    new file

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_number - print numbers chars
+ * @n: integer params
+ * Return: 0
+ */
+
+void print_number(int n)
+{
+	unsigned int a1;
+
+	a1 = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		a1 = -n;
+	}
+
+	if (a1 / 10 != 0)
+	{
+		print_number(a1 / 10);
+	}
+	_putchar((a1 % 10) + '0');
+}
+
